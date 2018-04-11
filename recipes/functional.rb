743f5a1f09ab032c99c03052b19fe6c3d9516f02
devoptimist <stephenb583@gmail.com>
2018-04-11 19:29:43 +0100

    t

--- recipes/functional.rb
+++ recipes/functional.rb
@@ -1,4 +1,4 @@
 execute "kitchen_#{node['delivery']['cookbook']['path']}" do
   cwd node['delivery']['cookbook']['path']
-  command "kitchen test"
+  command "chef exec kitchen test"
 end
--- recipes/unit.rb
+++ recipes/unit.rb
@@ -1,4 +1,4 @@
 execute "unit_rspec_#{node['delivery']['cookbook']['path']}" do
   cwd node['delivery']['cookbook']['path']
-  command "rspec --format documentation --color"
+  command "chef exec rspec --format documentation --color"
 end
