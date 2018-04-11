73ed08bc9bb812f0181c58f8d50ecea0afb6cd14
devoptimist <stephenb583@gmail.com>
2018-04-11 17:20:20 +0100

    t

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,4 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'fedora'
 issues_url 'https://github.com/devoptimist/my-delivery-truck/issues'
 source_url 'https://github.com/devoptimist/my-delivery-truck'
+depends 'delivery-sugar', '~> 1.1'
